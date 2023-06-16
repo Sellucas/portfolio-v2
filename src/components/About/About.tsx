300e8394a09479cf1e585414a6b904d163c43afd
Sellucas <lucassellmachado@gmail.com>
2023-06-16 12:41:55 -0300

    feat: about and footer section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,98 @@
-import React from 'react'
-import scss from './About.module.scss'
+import React from "react";
+import scss from "./About.module.scss";
+import {
+  SiNextdotjs,
+  SiMui,
+  SiTypescript,
+  SiReact,
+  SiJavascript,
+  SiSass,
+  SiTailwindcss,
+  SiBootstrap,
+  SiGit,
+  SiJest,
+  SiHtml5,
+  SiCss3,
+} from "react-icons/si";
+import Marquee from "react-fast-marquee";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className={scss.section}>
+      <div className={scss.customShapeDivider}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1200 0L0 0 598.97 114.72 1200 0z"
+            className={scss.shapeFill}
+          ></path>
+        </svg>
+      </div>
+      <div className={scss.container}>
+        <div className={scss.aboutMe}>
+          <h1>Oi, meu nome é Lucas. Prazer em te conhecer.</h1>
+          <p>
+            Sou desenvolvedor Front-End, tenho uma paixão por criar websites
+            bonitos e intuitivos. Com uma base sólida em ReactJs, estou
+            constantemente explorando novas tecnologias e tendências de design
+            para trazer ideias inovadoras à vida. Sou motivado e entusiasmado
+            com o desenvolvimento web, sempre em busca de aprendizado e
+            crescimento contínuos. Atualmente, estou me aprofundando nos estudos
+            de <span>Typescript e Jest</span>.
+          </p>
+        </div>
+        <div className={scss.stack}>
+          <Marquee
+            autoFill
+            gradient
+            gradientColor={[57, 27, 87]}
+            gradientWidth={800}
+          >
+            <span>
+              <SiReact />
+            </span>
+            <span>
+              <SiTypescript />
+            </span>
+            <span>
+              <SiJavascript />
+            </span>
+            <span>
+              <SiCss3 />
+            </span>
+            <span>
+              <SiSass />
+            </span>
+            <span>
+              <SiTailwindcss />
+            </span>
+            <span>
+              <SiBootstrap />
+            </span>
+            <span>
+              <SiMui />
+            </span>
+            <span>
+              <SiNextdotjs />
+            </span>
+            <span>
+              <SiGit />
+            </span>
+            <span>
+              <SiJest />
+            </span>
+            <span>
+              <SiHtml5 />
+            </span>
+          </Marquee>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import scss from './Footer.module.scss'
+import React from "react";
+import scss from "./Footer.module.scss";
+import { BsRocketFill } from "react-icons/bs";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { IoDocumentText } from "react-icons/io5";
+import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section className={scss.section}>
+      <div className={scss.box}>
+        <h2>Iniciar um projeto</h2>
+        <p>
+          Interessado em trabalhar juntos? <br />
+          Seria ótimo conversarmos.
+        </p>
+        <button>
+          <span>
+            <BsRocketFill />
+          </span>
+          Vamos fazer isso
+        </button>
+      </div>
+      <div className={scss.container}>
+        <h1>Em constante aprendizado. Um dia de cada vez.</h1>
+        <ul className={scss.socials}>
+          <li>
+            <a href="/">
+              <BsLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <BsGithub />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <SiGmail />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <IoDocumentText />
+            </a>
+          </li>
+        </ul>
+        <span className={scss.credits}>
+          Designed & Built by <a href="/">Sellucas</a>
+        </span>
+      </div>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
