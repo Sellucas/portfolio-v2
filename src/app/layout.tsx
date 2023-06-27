712f5799b9075a34f1cef0310c90e6029a46cc88
Sellucas <lucassellmachado@gmail.com>
2023-06-27 18:00:07 -0300

    fix: metahead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["100", "400", "700", "800"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "400", "700", "800"],
+});
 
 export default function RootLayout({
   children,
@@ -9,8 +13,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>Lucas Sell Machado | Frontend Developer</title>
+        <meta name="description" content="Data Dashboard" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </>
   );
 }
